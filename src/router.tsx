bb35e7656b8d7f608957d59e6c8a2c8c5aa14dcf
katesan945 <katesan945@gmail.com>
2025-11-14 00:44:46 +0700

    feat: update router to include custom 404 and loading components

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
-import { createRouter } from "@tanstack/react-router";
-
+import { createRouter } from "@tanstack/react-router"
+import { Loader2 } from "lucide-react"
 // Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+import { routeTree } from "./routeTree.gen"
 
 // Create a new router instance
 export const getRouter = () => {
@@ -9,7 +9,30 @@ export const getRouter = () => {
 		routeTree,
 		scrollRestoration: true,
 		defaultPreloadStaleTime: 0,
-	});
+		defaultNotFoundComponent: () => {
+			return (
+				<div className="h-full flex items-center justify-center flex-col my-auto">
+					<div className="my-auto">
+						<h1 className="text-center">
+							<span className="text-6xl font-bold mr-2 block mb-1">404</span>
+							ไม่พบหน้าที่คุณกำลังค้นหา
+						</h1>
+					</div>
+				</div>
+			)
+		},
+		defaultPendingComponent: () => (
+			<div className="h-full flex items-center justify-center flex-col my-auto">
+				<div className="my-auto">
+					<Loader2
+						className="animate-spin mx-auto size-24 mb-4"
+						size={48}
+					/>
+					<div className="text-center text-lg">กำลังโหลด...</div>
+				</div>
+			</div>
+		)
+	})
 
-	return router;
-};
+	return router
+}
