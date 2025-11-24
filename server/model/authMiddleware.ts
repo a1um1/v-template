44705e78d5551be932db48bbd88404346c223855
katesan945 <katesan945@gmail.com>
2025-11-24 22:19:18 +0700

    refactor: update VSCode settings and remove biome.json; add oxlint dependency

--- server/model/authMiddleware.ts
+++ server/model/authMiddleware.ts
@@ -3,35 +3,32 @@ import { Role } from "@server/permission";
 import { Session, User } from "better-auth";
 import Elysia from "elysia";
 
-type functionPermitions = NonNullable<NonNullable<Parameters<typeof auth.api.userHasPermission>>[0]>['body']
+type functionPermitions = NonNullable<
+  NonNullable<Parameters<typeof auth.api.userHasPermission>>[0]
+>["body"];
 
-export const authMiddleware = new Elysia({ name: "better-auth" })
-  .mount(auth.handler)
-  .macro({
-		auth: (config?: {
-      enabled?: boolean,
-      permissions?: NonNullable<functionPermitions['permissions']>
-    }) => config?.enabled || config?.permissions ? ({
-      async resolve({ status, request }): Promise<{
-        session: Session
-        user: User
-      } | {}> {
-       const data = await auth.api.getSession(request)
-       if (!data?.session) return status(401);
-       if (config?.permissions) {
-        const {success} = await auth.api.userHasPermission({
+export const authMiddleware = new Elysia({ name: "better-auth" }).mount(auth.handler).macro({
+  auth: (config?: NonNullable<functionPermitions["permissions"]>) => ({
+    async resolve({ status, request }): Promise<{
+      session: Session;
+      user: User;
+    }> {
+      const data = await auth.api.getSession(request);
+      if (!data?.session) throw new Error("Unauthorized");
+      if (config && Object.keys(config).length > 0) {
+        const { success } = await auth.api.userHasPermission({
           body: {
-            'userId': data.user.id,
-            'permissions': config.permissions,
-            'role': data.user.role as Role
-          }
-        })
-        if (!success) return status(403)
-       }
-        return {
-          session: data.session,
-          user: data.user
-        }
-		}}) : {
-    }
-	})
+            userId: data.user.id,
+            permissions: config,
+            role: data.user.role as Role,
+          },
+        });
+        if (!success) throw new Error("Unauthorized");
+      }
+      return {
+        session: data.session,
+        user: data.user,
+      };
+    },
+  }),
+});
--- server/routes/admin/index.ts
+++ server/routes/admin/index.ts
@@ -1,31 +1,33 @@
 import { authMiddleware } from "@server/model/authMiddleware";
 import Elysia, { t } from "elysia";
 
-const AdminRoute = new Elysia<'/admin'>({
-	prefix: '/admin'
+const AdminRoute = new Elysia<"/admin">({
+  prefix: "/admin",
 })
-	.use(authMiddleware)
-	.guard({
-		auth: {
-			'permissions': {
-				'adminDashboard': ['view']
-			}
-		}
-	})
-	.get('/', () => {
-		return {
-			message: 'Welcome to the admin dashboard'
-		}
-	})
-	.post('/settings', () => {
-		return {
-			message: 'Admin settings updated'
-		}
-	}, {
-		body: t.Object({
-			settingName: t.String(),
-			settingValue: t.String()
-		})
-	});
+  .use(authMiddleware)
+  .guard({
+    auth: {
+      adminDashboard: ["view"],
+    },
+  })
+  .get("/", () => {
+    return {
+      message: "Welcome to the admin dashboard",
+    };
+  })
+  .post(
+    "/settings",
+    ({ body }) => {
+      return {
+        message: "Admin settings updated",
+      };
+    },
+    {
+      body: t.Object({
+        settingName: t.String(),
+        settingValue: t.String(),
+      }),
+    },
+  );
 
 export default AdminRoute;
